696ea1f9d7c1043d7f718e6888376a7e417ee872
Brian Lu <me@greencappuccino.net>
2024-07-08 11:36:30 -0400

    work

--- src/lib.rs
+++ src/lib.rs
@@ -1,34 +1,189 @@
-use pgrx::prelude::*;
+use std::{ffi::{CStr, CString, OsStr, OsString}, fmt::format, mem::size_of, path::Path, str::FromStr};
+
+use pgrx::{prelude::*, set_varsize_4b, PgRelation};
 
 pgrx::pg_module_magic!();
 
-#[pg_extern]
-fn hello_pgmumbo() -> &'static str {
-    "Hello, pgmumbo"
+#[pg_extern(sql = "
+    CREATE OR REPLACE FUNCTION pgmumbo_amhandler(internal)
+        RETURNS index_am_handler
+        PARALLEL SAFE IMMUTABLE STRICT
+        COST 0.0001
+        LANGUAGE c
+        AS 'MODULE_PATHNAME', '@FUNCTION_NAME@';
+    CREATE ACCESS METHOD pgmumbo 
+        TYPE INDEX
+        HANDLER pgmumbo_amhandler;
+")]
+fn amhandler(_fcinfo: pg_sys::FunctionCallInfo) -> PgBox<pg_sys::IndexAmRoutine> {
+    let mut amroutine =
+        unsafe { PgBox::<pg_sys::IndexAmRoutine>::alloc_node(pg_sys::NodeTag::T_IndexAmRoutine) };
+
+    amroutine.amstrategies = 4;
+    amroutine.amsupport = 0;
+    amroutine.amcanmulticol = true;
+    amroutine.amsearcharray = true;
+
+    amroutine.amkeytype = pg_sys::InvalidOid;
+
+    amroutine.ambuild = Some(ambuild);
+    amroutine.ambuildempty = Some(ambuildempty);
+    amroutine.aminsert = Some(aminsert);
+    amroutine.ambulkdelete = Some(ambulkdelete);
+    amroutine.amvacuumcleanup = Some(amvacuumcleanup);
+    amroutine.amcostestimate = Some(amcostestimate);
+    amroutine.amoptions = Some(amoptions);
+    amroutine.amvalidate = Some(amvalidate);
+    amroutine.ambeginscan = Some(ambeginscan);
+    amroutine.amrescan = Some(amrescan);
+    // amroutine.amgettuple = Some(amgettuple);
+    // amroutine.amgetbitmap = Some(ambitmapscan);
+    amroutine.amendscan = Some(amendscan);
+
+    amroutine.into_pg_boxed()
 }
 
-#[cfg(any(test, feature = "pg_test"))]
-#[pg_schema]
-mod tests {
-    use pgrx::prelude::*;
+const INITIAL_LMDB_MMAP_SIZE: usize = 64 * 1024 * 1024 * 1024; // Initialize LMDB with 64 GB memory map
 
-    #[pg_test]
-    fn test_hello_pgmumbo() {
-        assert_eq!("Hello, pgmumbo", crate::hello_pgmumbo());
-    }
-
+#[repr(C)]
+struct MumboIndexOptionsData {
+    vl_len_: i32, // Postgres varlena header
+    relpath_offset: i32, // Offset
 }
 
-/// This module is required by `cargo pgrx test` invocations.
-/// It must be visible at the root of your extension crate.
-#[cfg(test)]
-pub mod pg_test {
-    pub fn setup(_options: Vec<&str>) {
-        // perform one-off initialization when the pg_test framework starts
-    }
-
-    pub fn postgresql_conf_options() -> Vec<&'static str> {
-        // return any postgresql.conf settings that are required for your tests
-        vec![]
-    }
+fn pgdata_path() -> CString {
+    unsafe { CStr::from_ptr(pg_sys::DataDir) }.into()
 }
+
+#[pg_guard]
+pub extern "C" fn ambuild(
+    heap_relation: pg_sys::Relation,
+    index_relation: pg_sys::Relation,
+    index_info: *mut pg_sys::IndexInfo,
+) -> *mut pg_sys::IndexBuildResult {
+    info!("pgmumbo ambuild");
+
+    let heap_relation = unsafe { PgRelation::from_pg(heap_relation) };
+    let index_relation = unsafe { PgRelation::from_pg(index_relation) };
+
+    assert!(index_relation.is_index());
+
+    let index_options = if index_relation.rd_options.is_null() {
+        let options = unsafe { PgBox::<MumboIndexOptionsData>::alloc0() };
+        unsafe {
+            set_varsize_4b(
+                options.as_ptr().cast(),
+                size_of::<MumboIndexOptionsData>() as i32,
+            );
+        }
+        options.into_pg_boxed()
+    } else {
+        unsafe { PgBox::from_pg(index_relation.rd_options as *mut MumboIndexOptionsData) }
+    };
+
+    let index_oid = index_relation.oid();
+
+    let PGDATA = ;
+
+    todo!()
+}
+
+#[pg_guard]
+pub extern "C" fn ambuildempty(_index_relation: pg_sys::Relation) {
+    info!("pgmumbo ambuildempty");
+}
+
+#[pg_guard]
+pub unsafe extern "C" fn aminsert(
+    index_relation: pg_sys::Relation,
+    values: *mut pg_sys::Datum,
+    _isnull: *mut bool,
+    heap_tid: pg_sys::ItemPointer,
+    _heap_relation: pg_sys::Relation,
+    _check_unique: pg_sys::IndexUniqueCheck::Type,
+    _index_unchanged: bool,
+    _index_info: *mut pg_sys::IndexInfo,
+) -> bool {
+    info!("pgmumbo aminsert");
+
+    todo!()
+}
+
+#[pg_guard]
+pub extern "C" fn ambulkdelete(
+    info: *mut pg_sys::IndexVacuumInfo,
+    stats: *mut pg_sys::IndexBulkDeleteResult,
+    _callback: pg_sys::IndexBulkDeleteCallback,
+    _callback_state: *mut ::std::os::raw::c_void,
+) -> *mut pg_sys::IndexBulkDeleteResult {
+    todo!()
+}
+
+#[pg_guard]
+pub extern "C" fn amvacuumcleanup(
+    info: *mut pg_sys::IndexVacuumInfo,
+    stats: *mut pg_sys::IndexBulkDeleteResult,
+) -> *mut pg_sys::IndexBulkDeleteResult {
+    todo!()
+}
+
+#[pg_guard(immutable, parallel_safe)]
+pub unsafe extern "C" fn amcostestimate(
+    _root: *mut pg_sys::PlannerInfo,
+    path: *mut pg_sys::IndexPath,
+    _loop_count: f64,
+    index_startup_cost: *mut pg_sys::Cost,
+    index_total_cost: *mut pg_sys::Cost,
+    index_selectivity: *mut pg_sys::Selectivity,
+    index_correlation: *mut f64,
+    index_pages: *mut f64,
+) {
+}
+
+#[pg_guard]
+pub unsafe extern "C" fn amoptions(
+    reloptions: pg_sys::Datum,
+    validate: bool,
+) -> *mut pg_sys::bytea {
+    todo!()
+}
+
+#[pg_guard]
+pub extern "C" fn amvalidate(_opclassoid: pg_sys::Oid) -> bool {
+    true
+}
+
+#[pg_guard]
+pub extern "C" fn ambeginscan(
+    index_relation: pg_sys::Relation,
+    nkeys: ::std::os::raw::c_int,
+    norderbys: ::std::os::raw::c_int,
+) -> pg_sys::IndexScanDesc {
+    todo!()
+}
+
+#[pg_guard]
+pub extern "C" fn amrescan(
+    scan: pg_sys::IndexScanDesc,
+    keys: pg_sys::ScanKey,
+    nkeys: ::std::os::raw::c_int,
+    _orderbys: pg_sys::ScanKey,
+    _norderbys: ::std::os::raw::c_int,
+) {
+}
+
+// #[pg_guard]
+// pub extern "C" fn amgettuple(
+//     scan: pg_sys::IndexScanDesc,
+//     _direction: pg_sys::ScanDirection,
+// ) -> bool {
+//     todo!()
+// }
+
+// #[pg_guard]
+// pub extern "C" fn ambitmapscan(scan: pg_sys::IndexScanDesc, tbm: *mut pg_sys::TIDBitmap) -> i64 {
+//     todo!()
+// }
+
+#[pg_guard]
+pub extern "C" fn amendscan(_scan: pg_sys::IndexScanDesc) {}
