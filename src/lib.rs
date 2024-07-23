d3ca6f20188120d6886e30bae8f9658ccc40bb4c
Brian Lu <me@greencappuccino.net>
2024-07-23 11:07:34 -0400

    initial encode options

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,13 @@
 use std::{
+    collections::{BTreeMap, BTreeSet, HashSet},
+    convert::Infallible,
     ffi::CStr,
     fs,
     io::Cursor,
     iter::{self, zip},
     mem,
     num::ParseIntError,
+    option,
     os::raw,
     path::Path,
     ptr, slice,
@@ -12,18 +15,22 @@ use std::{
     string::ParseError,
 };
 
+use lazy_static::lazy_static;
 use milli::{
     documents::{DocumentsBatchBuilder, DocumentsBatchReader},
     heed::EnvOpenOptions,
+    order_by_map::OrderByMap as MilliOrderByMap,
     update::{
         IndexDocuments as MilliIndexDocuments, IndexDocumentsConfig as MilliIndexDocumentsConfig,
         IndexDocumentsMethod as MilliIndexDocumentsMethod, IndexerConfig as MilliIndexerConfig,
         Settings as MilliSettings,
     },
-    Index as MilliIndex,
+    Criterion as MilliCriterion, Index as MilliIndex,
 };
 use pg_sys::{palloc0, panic::ErrorReportable, FormData_pg_attribute, ItemPointerData, Oid};
 use pgrx::{prelude::*, PgMemoryContexts, PgRelation};
+use serde::{Deserialize, Serialize};
+use serde_json::Value;
 use thiserror::Error;
 
 pgrx::pg_module_magic!();
@@ -202,7 +209,7 @@ unsafe extern "C" fn build_callback(
     let attrs = desc.attrs.as_slice(natts);
     let values = slice::from_raw_parts(values, natts);
 
-    let document = form_document(TID(tid), isnull, attrs, values);
+    let document = form_document(Tid(tid), isnull, attrs, values);
 
     info!("pgmumbo heapscan: {document:?}");
 
@@ -215,9 +222,9 @@ unsafe extern "C" fn build_callback(
     build_state.owned_context.reset();
 }
 
-struct TID(*mut pg_sys::ItemPointerData);
+struct Tid(*mut pg_sys::ItemPointerData);
 
-impl ToString for TID {
+impl ToString for Tid {
     fn to_string(&self) -> String {
         format!(
             "{}-{}_{}",
@@ -227,27 +234,25 @@ impl ToString for TID {
         )
     }
 }
-impl FromStr for TID {
-    type Err = PgmumboError;
+impl FromStr for Tid {
+    type Err = Error;
 
     fn from_str(s: &str) -> Result<Self, Self::Err> {
-        let (blk_str, pos_str) = s.split_once("_").ok_or(PgmumboError::TIDSplit)?;
-        let (blk_hi_str, blk_lo_str) = blk_str.split_once("-").ok_or(PgmumboError::TIDSplit)?;
+        let (blk_str, pos_str) = s.split_once("_").ok_or(Error::TIDSplit)?;
+        let (blk_hi_str, blk_lo_str) = blk_str.split_once("-").ok_or(Error::TIDSplit)?;
         let item_ptr: *mut ItemPointerData =
             unsafe { palloc0(mem::size_of::<pg_sys::ItemPointerData>()).cast() };
 
-        unsafe { *item_ptr }.ip_blkid.bi_hi =
-            blk_hi_str.parse().map_err(PgmumboError::TIDParseInt)?;
-        unsafe { *item_ptr }.ip_blkid.bi_lo =
-            blk_lo_str.parse().map_err(PgmumboError::TIDParseInt)?;
-        unsafe { *item_ptr }.ip_posid = pos_str.parse().map_err(PgmumboError::TIDParseInt)?;
+        unsafe { *item_ptr }.ip_blkid.bi_hi = blk_hi_str.parse().map_err(Error::TIDParseInt)?;
+        unsafe { *item_ptr }.ip_blkid.bi_lo = blk_lo_str.parse().map_err(Error::TIDParseInt)?;
+        unsafe { *item_ptr }.ip_posid = pos_str.parse().map_err(Error::TIDParseInt)?;
 
-        Ok(TID(item_ptr))
+        Ok(Tid(item_ptr))
     }
 }
 
 fn form_document(
-    tid: TID,
+    tid: Tid,
     isnull: &[bool],
     attrs: &[pg_sys::FormData_pg_attribute],
     values: &[pg_sys::Datum],
@@ -340,11 +345,44 @@ pub unsafe extern "C" fn amcostestimate(
 ) {
 }
 
+#[derive(Default, Serialize, Deserialize)]
 struct PgmumboOptions {
-    milli_words_prefix_threshold: Option<u32>,
-    milli_max_prefix_length: Option<u32>,
-    milli_words_positions_level_group_size: Option<u32>,
-    milli_words_positions_min_level_size: Option<u32>,
+    // Unmanaged MilliSettings fields
+    m_searchable_fields: Option<Vec<String>>,
+    m_displayed_fields: Option<Vec<String>>,
+    m_filterable_fields: Option<HashSet<String>>,
+    m_sortable_fields: Option<HashSet<String>>,
+    m_criteria: Option<Vec<MilliCriterion>>,
+    m_stop_words: Option<BTreeSet<String>>,
+    m_non_separator_tokens: Option<BTreeSet<String>>,
+    m_separator_tokens: Option<BTreeSet<String>>,
+    m_dictionary: Option<BTreeSet<String>>,
+    m_distinct_field: Option<String>,
+    m_synonyms: Option<BTreeMap<String, Vec<String>>>,
+    m_authorize_typos: Option<bool>,
+    m_min_word_len_two_typos: Option<u8>,
+    m_min_word_len_one_typo: Option<u8>,
+    m_exact_words: Option<BTreeSet<String>>,
+    m_exact_attributes: Option<HashSet<String>>,
+    m_max_values_per_facet: Option<usize>,
+    m_sort_facet_values_by: Option<MilliOrderByMap>,
+    m_pagination_max_total_hits: Option<usize>,
+    m_proximity_precision: Option<usize>,
+    m_search_cutoff: Option<u64>,
+    // Unmanaged MilliIndexDocumentsConfig fields
+    m_doc_words_prefix_threshold: Option<u32>,
+    m_doc_max_prefix_length: Option<u32>,
+    m_doc_words_positions_level_group_size: Option<u32>,
+    m_doc_words_positions_min_level_size: Option<u32>,
+}
+
+lazy_static! {
+    static ref options_field_names: HashSet<String> =
+        if let Value::Object(map) = serde_json::to_value(PgmumboOptions::default()).unwrap() {
+            map.keys().cloned().collect()
+        } else {
+            panic!()
+        };
 }
 
 #[pg_guard]
@@ -377,20 +415,71 @@ pub unsafe extern "C" fn amoptions(
     let elems = slice::from_raw_parts(elemsp, nelem);
     let nulls = slice::from_raw_parts(nullsp, nelem);
 
-    let pairs = zip(nulls, elems).filter_map(|(null, elem)| {
-        if *null {
-            return None;
+    let bytes = match encode_options(validate, elems, nulls) {
+        Ok(bytes) => bytes,
+        Err(error) => {
+            ereport!(
+                ERROR,
+                PgSqlErrorCode::ERRCODE_INVALID_PARAMETER_VALUE,
+                error.to_string()
+            );
         }
-        let pair = unsafe { CStr::from_ptr(pg_sys::text_to_cstring(elem.cast_mut_ptr())) }
-            .to_str()
-            .unwrap_or_report()
-            .split_once("=")
-            .unwrap();
-        Some(pair)
-    });
+    };
 
-    // let reloptions = std::slice::from_raw_parts(reloptions, len);
-    ptr::null_mut()
+    let encoded_size = mem::size_of::<[::std::os::raw::c_char; 4usize]>() + bytes.len();
+    let encoded = pg_sys::palloc0(encoded_size) as *mut pg_sys::bytea;
+
+    (*encoded).vl_len_ = (encoded_size as i32).to_ne_bytes().map(|x| x as i8);
+    ptr::copy_nonoverlapping(
+        bytes.as_ptr(),
+        (*encoded).vl_dat.as_mut_ptr() as *mut u8,
+        bytes.len(),
+    );
+
+    encoded
+}
+
+fn encode_options(
+    validate: bool,
+    elems: &[pg_sys::Datum],
+    nulls: &[bool],
+) -> Result<Vec<u8>, Error> {
+    let pairs = zip(nulls, elems)
+        .filter_map(|(null, elem)| {
+            if *null {
+                return None;
+            }
+            let (k_str, v_str) =
+                unsafe { CStr::from_ptr(pg_sys::text_to_cstring(elem.cast_mut_ptr())) }
+                    .to_str()
+                    .unwrap_or_report()
+                    .split_once('=')
+                    .unwrap();
+            let k = k_str.to_string();
+            Some(serde_json::from_str::<serde_json::Value>(v_str).map(|v| (k, v)))
+        })
+        .collect::<Result<Vec<_>, _>>()
+        .map_err(Error::OptionsParseValue)?;
+    let options_map = serde_json::Map::from_iter(pairs);
+
+    if validate {
+        let difference: Vec<_> = options_map
+            .keys()
+            .filter(|key| !options_field_names.contains(*key))
+            .cloned()
+            .collect();
+        if !difference.is_empty() {
+            return Err(Error::OptionsInvalidKeys(difference));
+        }
+    }
+
+    let options: PgmumboOptions = serde_json::from_value(serde_json::Value::Object(options_map))
+        .map_err(Error::OptionsParseValue)?;
+
+    let mut bytes = Vec::new();
+    ciborium::into_writer(&options, &mut bytes).map_err(Error::OptionsEncode)?;
+
+    Ok(bytes)
 }
 
 #[pg_guard]
@@ -434,9 +523,15 @@ pub extern "C" fn amrescan(
 pub extern "C" fn amendscan(_scan: pg_sys::IndexScanDesc) {}
 
 #[derive(Error, Debug)]
-pub enum PgmumboError {
+pub enum Error {
     #[error("Failed to split TID serialization, it could be malformed")]
     TIDSplit,
     #[error("Failed to parse TID integer, it could be malformed")]
     TIDParseInt(ParseIntError),
+    #[error("Failed to parse index option value from JSON")]
+    OptionsParseValue(serde_json::Error),
+    #[error("Failed to encode index options to CBOR")]
+    OptionsEncode(ciborium::ser::Error<std::io::Error>),
+    #[error("Invalid index options keys")]
+    OptionsInvalidKeys(Vec<String>),
 }
